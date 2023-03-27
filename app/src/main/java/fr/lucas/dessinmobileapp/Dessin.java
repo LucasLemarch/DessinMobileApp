44d144f81438aa1e7707e7db6abb2fa2e4a700f2
Lucas Lemarchand <lucas.lemarchand2003@gmail.com>
2023-03-27 19:27:10 +0200

    Initial commit

---
+++ app/src/main/java/fr/lucas/dessinmobileapp/Dessin.java
@@ -0,0 +1,14 @@
+package fr.lucas.dessinmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dessin extends AppCompatActivity {
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dessin);
+	}
+}
