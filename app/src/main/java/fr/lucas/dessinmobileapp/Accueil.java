79ca6e2d477061f0e85172553eac2bb57ecb553d
Lucas Lemarchand <lucas.lemarchand2003@gmail.com>
2023-03-27 22:38:52 +0200

    Debut mise en page Activité Dessin

--- app/src/main/java/fr/lucas/dessinmobileapp/Accueil.java
+++ app/src/main/java/fr/lucas/dessinmobileapp/Accueil.java
@@ -2,7 +2,9 @@ package fr.lucas.dessinmobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Accueil extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Accueil extends AppCompatActivity {
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_accueil);
+	}
+
+	public void nouveauDessin(View v) {
+		Intent intent = new Intent(Accueil.this, Dessin.class);
+		Accueil.this.startActivity(intent);
 	}
 }
--- app/src/main/java/fr/lucas/dessinmobileapp/Dessin.java
+++ app/src/main/java/fr/lucas/dessinmobileapp/Dessin.java
@@ -1,14 +1,35 @@
 package fr.lucas.dessinmobileapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
 
 public class Dessin extends AppCompatActivity {
+
+	DrawerLayout layoutOutils;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dessin);
+
+		layoutOutils = findViewById(R.id.choixOutils);
+
+
+	}
+
+
+	public void ouvrirOutils(View v) {
+		layoutOutils.open();
+	}
+
+	public void fermerOutils(View v) {
+		layoutOutils.close();
 	}
 }
